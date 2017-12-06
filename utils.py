737611389073b8d3b4ce2c0917364aedf64141f4
dada8397 <vincent8397@gmail.com>
2017-12-06 14:10:03 +0800

    Modify parameter name of next_batch

--- utils.py
+++ utils.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
 import numpy as np
 
-def next_batch(x, y, size, times):
+def next_batch(x, y, batch_size, times):
     global z, x1, y1
     if times == 1:
         combined = np.c_[x.reshape(len(x), -1), y.reshape(len(y), -1)]
         x1 = combined[:, :x.size//len(x)].reshape(x.shape)
         y1 = combined[:, x.size//len(x):].reshape(y.shape)
-    
-    curr = times % (y.size//size)
+
+    curr = times % (y.size//batch_size)
     if curr == 0:
-        curr = y.size//size
+        curr = y.size//batch_size
 
     if curr == 1:
         np.random.shuffle(combined)
 
-    return x1[size*(curr-1):size*curr], y1[size*(curr-1):size*curr]
+    return x1[batch_size*(curr-1):batch_size*curr], y1[batch_size*(curr-1):batch_size*curr]
 
