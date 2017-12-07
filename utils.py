0b2c710a8bd91d771130a6ca0d5f9fc6935a6ebf
dada8397 <vincent8397@gmail.com>
2017-12-07 09:26:05 +0800

    Modify batch_next

--- utils.py
+++ utils.py
@@ -6,12 +6,12 @@ y = np.load('data/train_data_Y.npy')
 
 def next_batch(batch_size, times):
     global z, x1, y1
-    if times == 1:
+    if times == 0:
         combined = np.c_[x.reshape(len(x), -1), y.reshape(len(y), -1)]
         x1 = combined[:, :x.size//len(x)].reshape(x.shape)
         y1 = combined[:, x.size//len(x):].reshape(y.shape)
 
-    curr = times % (y.size//batch_size)
+    curr = (times + 1) % (y.size//batch_size)
     if curr == 0:
         curr = y.size//batch_size
 
