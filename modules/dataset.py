98383892f2ad72297f3f4a440a6263e84bc1d5e7
uno <st9007a@gmail.com>
2018-01-07 21:16:43 +0800

    Optimize onehot21 encoding
    Use parallel iteration

--- modules/dataset.py
+++ modules/dataset.py
@@ -5,9 +5,12 @@ import sys
 def _encode_onehot21(x):
     x = tf.one_hot(x, depth = 21)
     x = tf.reshape(x, [33, 21])
-    x = tf.unstack(x, axis = 0)
-    x = [tf.where(tf.equal(tf.reduce_sum(t), 0), tf.constant([0.05] * 20 + [0], tf.float32), t) for t in x]
-    x = tf.stack(x, axis = 0)
+    x = tf.map_fn(
+        lambda t: tf.where(tf.equal(tf.reduce_sum(t), 0), tf.constant([0.05] * 20 + [0], tf.float32), t), \
+        elems = x, \
+        dtype = tf.float32, \
+        parallel_iterations = 10 \
+    )
 
     return x
 
@@ -47,8 +50,14 @@ else:
 
 if __name__ == '__main__':
 
-    test = tf.constant([1, 20, 21, 10], tf.uint8)
-    test = _encode_onehot21(test)
+    constant = [ 1,  2,  3,  4,  5,  6,  7,  8,  9, 10, \
+                11, 12, 13, 14, 15, 16, 17, 18, 19, 20, \
+                21,  1,  2,  3,  4,  5,  6,  7,  8,  9, \
+                10, 11, 13]
+
+    with tf.device('/cpu:0'):
+        test = tf.constant(constant, tf.uint8)
+        test = _encode_onehot21(test)
     sess = tf.Session()
     print(sess.run(test))
 
