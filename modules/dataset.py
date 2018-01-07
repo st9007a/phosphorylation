1a4a701bb1002ba8bb4531d8441c1a8cac1a9925
uno <st9007a@gmail.com>
2018-01-07 22:30:10 +0800

    Remove tf.map_fn

--- modules/dataset.py
+++ modules/dataset.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
 import tensorflow as tf
-import sys
 
 def _encode_onehot21(x):
     x = tf.one_hot(x, depth = 21)
     x = tf.reshape(x, [33, 21])
-    x = tf.map_fn(
-        lambda t: tf.where(tf.equal(tf.reduce_sum(t), 0), tf.constant([0.05] * 20 + [0], tf.float32), t), \
-        elems = x, \
-        dtype = tf.float32, \
-        parallel_iterations = 10 \
-    )
+    x = tf.unstack(x, axis = 0)
+    x = [tf.where(tf.equal(tf.reduce_sum(t), 0), tf.constant([0.05] * 20 + [0], tf.float32), t) for t in x]
+    x = tf.stack(x, axis = 0)
+    # x = tf.map_fn(
+    #     lambda t: tf.where(tf.equal(tf.reduce_sum(t), 0), tf.constant([0.05] * 20 + [0], tf.float32), t), \
+    #     elems = x, \
+    #     dtype = tf.float32, \
+    #     parallel_iterations = 10 \
+    # )
 
     return x
 
@@ -33,20 +35,12 @@ def _encode_identity(x):
     return tf.cast(x, tf.float32)
 
 encode_func_map = {
+    'identity': _encode_identity,
     'onehot21': _encode_onehot21,
     'onehot22': _encode_onehot22,
     'onehot21_nox': _encode_onehot21_nox,
     'onehot21_nopad': _encode_onehot21_nopad,
 }
-
-encode_func = _encode_identity
-
-if len(sys.argv) < 2:
-    print('Pipeline without encoding')
-else:
-    print('Pipeline with ' + sys.argv[1] + ' encoding')
-    encode_func = encode_func_map[sys.argv[1]]
-
 
 if __name__ == '__main__':
 
@@ -62,30 +56,30 @@ if __name__ == '__main__':
     print(sess.run(test))
 
 
-def _parse_tfrecord(example_proto):
-    features = {
-        'x': tf.FixedLenFeature([], tf.string, default_value = ''),
-        'y': tf.FixedLenFeature([], tf.string, default_value = ''),
-    }
-
-    parsed_features = tf.parse_single_example(example_proto, features, name = 'parse_feature')
-
-    x = tf.decode_raw(parsed_features['x'], tf.uint8)
-    y = tf.decode_raw(parsed_features['y'], tf.uint8)
-    y = tf.one_hot(y, depth = 2)
-    y = tf.cast(y, tf.float32)
-    return encode_func(x), y
-
 class Dataset():
 
-    def __init__(self, trainfiles, testfiles, batch_size, parallel_call = 1):
+    def __init__(self, encode, trainfiles, testfiles, batch_size, parallel_call = 1):
 
         self.trainfiles = trainfiles
         self.testfiles = testfiles
+        self.encode_func = encode_func_map[encode]
 
         self.dataset = self.data_pipeline(batch_size, parallel_call)
         self.iterator = self.dataset.make_initializable_iterator()
 
+    def _parse_tfrecord(self, example_proto):
+        features = {
+            'x': tf.FixedLenFeature([], tf.string, default_value = ''),
+            'y': tf.FixedLenFeature([], tf.string, default_value = ''),
+        }
+
+        parsed_features = tf.parse_single_example(example_proto, features, name = 'parse_feature')
+
+        x = tf.decode_raw(parsed_features['x'], tf.uint8)
+        y = tf.decode_raw(parsed_features['y'], tf.uint8)
+        y = tf.one_hot(y, depth = 2)
+        y = tf.cast(y, tf.float32)
+        return self.encode_func(x), y
 
     def data_pipeline(self, batch_size, parallel_call):
 
@@ -94,7 +88,7 @@ class Dataset():
             self.files = tf.placeholder(tf.string, shape = [None])
 
             dataset = tf.data.TFRecordDataset(self.files)
-            dataset = dataset.map(_parse_tfrecord, num_parallel_calls = parallel_call)
+            dataset = dataset.map(self._parse_tfrecord, num_parallel_calls = parallel_call)
             dataset = dataset.shuffle(buffer_size = 10000)
             dataset = dataset.batch(batch_size)
 
