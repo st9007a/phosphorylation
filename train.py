79ae9f27f0b130136db5445aa44179f1d963ae1e
uno <st9007a@gmail.com>
2018-01-07 22:29:41 +0800

    Add argument parser in train.py

--- train.py
+++ train.py
@@ -1,19 +1,37 @@
 #!/usr/bin/python3
 import datetime
 import tensorflow as tf
+import os
+
+from argparse import ArgumentParser
+
 from modules.model import MusiteDeepModel
 from modules.dataset import Dataset
 
 if __name__ == '__main__':
 
+    arg_parser = ArgumentParser()
+    arg_parser.add_argument('--force', '-f', action = 'store_true', help = 'force to write log in LOGDIR')
+    arg_parser.add_argument('--logdir', '-l', help = 'setup tensorboard log directory')
+    arg_parser.add_argument('--encode', '-e', default = 'identity', help = 'setup encodeing function. default run without encode')
+
+    args = arg_parser.parse_args()
+    if args.logdir == None:
+        print('Need a logdir. See python3 train.py --help')
+        exit()
+    elif os.path.isdir(args.logdir) and not args.force:
+        print('logdir is exist')
+        exit()
+
     data = Dataset(
+        encode = args.encode, \
         trainfiles = ['data/baseline/train.tfrecord'], \
         testfiles = ['data/baseline/test.tfrecord'], \
         batch_size = 200, \
         parallel_call = 4 \
     )
 
-    model = MusiteDeepModel(dataset = data, logdir = 'log/baseline/onehot21-1')
+    model = MusiteDeepModel(dataset = data, logdir = args.logdir)
 
     start = datetime.datetime.now()
     model.train(epochs = 500)
